22854a41225393c1108d0b2283c8628c9d63e373
MML-GS9\kaihua <tkhchipaomian@gmail.com>
2018-08-11 12:07:56 +0800

    update

--- tools/h5tonp.py
+++ tools/h5tonp.py
@@ -3,18 +3,22 @@ import numpy as np
 import h5py
 
 if __name__ == '__main__':
+    print('Load val data')
     hf_val = h5py.File('data/val36.hdf5', 'r')
     image_features_val36 = np.array(hf_val.get('image_features'))
     spatials_val36 = np.array(hf_val.get('spatial_features'))
     del hf_val
+    print('Saving val data')
     np.save('image_features_val36.npy', image_features_val36)
     np.save('spatials_val36.npy', spatials_val36)
     del image_features_val36, spatials_val36
 
+    print('Load Train data')
     hf_train = h5py.File('data/train36.hdf5', 'r')
     image_features_train36 = np.array(hf_train.get('image_features'))
     spatials_train36 = np.array(hf_train.get('spatial_features'))
     del hf_train
+    print('Saving Train data')
     np.save('image_features_train36.npy', image_features_train36)
     np.save('spatials_train36.npy', spatials_train36)
     del image_features_train36, spatials_train36
